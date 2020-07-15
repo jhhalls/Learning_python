e65d6fe30b9b3f473bb6831fd174adc86ce1e8d6
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-15 22:35:27 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,28 @@
+"""
+Created on Wed Jul 15 01:43:07 2020
+
+@author: trendydice
+"""
+# Write a Python program to print the following 
+#string in a specific format
+# =============================================================================
+# Sample String : 
+#     Twinkle, twinkle, little star, 
+#     How I wonder what you are! 
+#     Up above the world so high, 
+#     Like a diamond in the sky. 
+#     Twinkle, twinkle, little star,
+#     How I wonder what you are" 
+# =============================================================================
+    
+# =============================================================================
+# Output :
+# Twinkle, twinkle, little star,
+#         How I wonder what you are! 
+#                 Up above the world so high, 
+#                 Like a diamond in the sky.
+# Twinkle, twinkle, little star, 
+#         How I wonder what you are" 
+# =============================================================================
+
+print('Twinkle, twinkle, little star,\n','\t\tHow I wonder what you are!\n', '\t\t\t\tUp above the world so high,\n\t\t\t\tLike a diamond in the sky.\n ', 'Twinkle, twinkle, little star,\n', '\t\tHow I wonder what you are')
