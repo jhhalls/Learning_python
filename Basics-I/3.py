e14c40946825864558eb3471ee8476d803d25e0e
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-16 21:22:11 +0530

    Add files via upload

---
+++ Basics-I/3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 01:59:36 2020
+
+@author: trendydice
+"""
+
+# =============================================================================
+# 3. Write a Python program to display the current date and time.
+# 
+# =============================================================================
+
+# current date and time
+
+import datetime
+print(datetime.datetime.now())
+
+birthday = datetime.date(2020,10,26)
+print(birthday,'birthday')
