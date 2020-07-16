2f6cabab091ef1ee0d1a02ac935a0146d468a449
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-16 21:19:49 +0530

    Add files via upload

---
+++ Basics-I/2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 01:55:31 2020
+
+@author: trendydice
+"""
+
+# Write a Python program to get the Python version you are using.
+
+
+
+import sys
+print('python version', sys.version)
+
+print(sys.version_info)
