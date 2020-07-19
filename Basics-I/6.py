512597b840e8cb7d2d95953010b25badd87144f2
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-19 20:53:56 +0530

    Add files via upload

---
+++ Basics-I/6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 02:11:30 2020
+
+@author: trendydice
+"""
+# =============================================================================
+# Write a Python program which accepts a sequence of comma-separated numbers 
+# from user and generate a list and a tuple with those numbers.
+# 
+# 
+# 
+# 
+# 
+# =============================================================================
+
+enter=input('Enter numbers: ')
+list=enter.split(',')
+tuple = tuple(list)
+print('tuple:', tuple)
+print('list:', list)
+
