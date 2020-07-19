1cbfd5b222911eabb7830e959831349ca8b761a2
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-19 20:53:03 +0530

    Add files via upload

---
+++ Basics-I/5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 02:06:35 2020
+
+@author: trendydice
+"""
+# =============================================================================
+# 
+# Write a Python program which accepts the user's first and last name
+# and print them in reverse order with a space between them.
+#
+# =============================================================================
+
+
+first = input('Enter First name: ')
+last = input('Enter Last name: ')
+print(last+' ' +first)
