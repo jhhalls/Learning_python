a036ecb22684ee58aa0c97412e87c81a4dc96da0
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-22 22:36:23 +0530

    Add files via upload

---
+++ Basics-I/8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 02:20:14 2020
+
+@author: trendydice
+"""
+
+# =============================================================================
+#  Write a Python program to display the first and last colors 
+#  from the following list. 
+#
+# color_list = ["Red","Green","White" ,"Black"]
+# 
+# 
+# 
+# 
+# =============================================================================
+
+
+color_list = ["Red","Green","White" ,"Black"]
+print('last_color', color_list[-1])
+print('first_color', color_list[0])
