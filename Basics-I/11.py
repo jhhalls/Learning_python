822f820bf350ac37f7514b67b21a057046de2081
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-24 23:08:26 +0530

    Add files via upload

---
+++ Basics-I/11.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 02:37:47 2020
+
+@author: trendydice
+"""
+
+# =============================================================================
+# Write a Python program to print the documents 
+# (syntax, description etc.) of Python built-in function(s).
+# Sample function : abs()
+# Expected Result :
+# abs(number) -> number
+# Return the absolute value of the argument.
+# =============================================================================
+
+
+sample_func = abs(2)
+print(sample_func.__doc__)
+
+
+
+
+
+
