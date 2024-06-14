126ceeb6dcf06e3383ea8f15529b3c1bca278f1d
Rohit Raj Jalheria <rjalheira@gmail.com>
2024-06-14 21:37:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# import the enttire module
+import module
+
+# import specific fucntion or classes
+from module import add,Greeter
+
+# use the functions
+result_add = module.add(2,4)
+result_subtract = module.subtract(10,5)
+
+print(f"Addition results : {result_add}")
+print(f"Subtraction results : {result_subtract}")
+
+# use the class
+greeter = Greeter("Alice")
+print(greeter.greet())
---
+++ module.py
@@ -0,0 +1,19 @@
+# mymodule.py
+
+def add(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Return the difference between two numbers."""
+    return a - b
+
+class Greeter:
+    """A simple class to greet people."""
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        """Return a greeting message."""
+        return f"Hello, {self.name}!"
