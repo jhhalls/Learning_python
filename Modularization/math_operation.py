a58440828e981f5181e4dd7aaa53c944591905e2
Rohit Raj Jalheria <rjalheira@gmail.com>
2024-06-15 17:46:22 +0530

    Add files via upload

---
+++ Modularization/math_operation.py
@@ -0,0 +1,14 @@
+def multiply(a,b):
+	"""" Returns the multiplication of the inputs"""
+	return a*b
+
+def divide(a,b):
+	"""" Returns the multiplication of the inputs"""
+	if b==0:
+		raise ZeroDivisionError
+	return a/b
+
+def modulus(a,b):
+	"""" Returns the multiplication of the inputs"""
+	return a%b
+
